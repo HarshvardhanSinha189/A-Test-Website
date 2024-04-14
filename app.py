6edd39f8277114bcc6ce044ac4eddda1de27ed6a
HarshvardhanSinha189 <166268078+HarshvardhanSinha189@users.noreply.github.com>
2024-04-14 12:28:59 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello You are here! about page <a href="/about"> about </a> '
+
+@app.route('/about')
+def hello_world():
+    return 'Welcome to the about page '
